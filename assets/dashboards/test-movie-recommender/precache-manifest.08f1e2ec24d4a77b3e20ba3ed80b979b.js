34e92453f0cee5c3cf435d446d1bd278e5504f0a
Michael Richman <mike.d.richman@gmail.com>
2020-08-11 00:50:10 -0400

    update "homepage" path

---
+++ assets/dashboards/test-movie-recommender/precache-manifest.08f1e2ec24d4a77b3e20ba3ed80b979b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64554b866e55b64e8acc613bc7a6afa7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "295943d0c52ad08392d7",
+    "url": "./static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "c0de52fe6639b90efb32",
+    "url": "./static/css/main.dbc33782.chunk.css"
+  },
+  {
+    "revision": "295943d0c52ad08392d7",
+    "url": "./static/js/2.062acf34.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.062acf34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0de52fe6639b90efb32",
+    "url": "./static/js/main.accce487.chunk.js"
+  },
+  {
+    "revision": "437ffda59c3336941391",
+    "url": "./static/js/runtime-main.de4efd7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
